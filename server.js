04164fb18a79545b69e613319040d37574ab1f8c
Yoni Water Man <yonixw@gmail.com>
2020-09-02 16:05:57 +0300

    save last call to temp.json

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const converter = require("./lib")
 const { json } = require('body-parser')
 const fetch = require('node-fetch');
+const fs = require('fs');
 
 
 const app = express()
@@ -18,12 +19,20 @@ app.use((req, res, next) => {
     res.set("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
     res.set("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
     res.set("Access-Control-Allow-Headers", "Content-type,Accept");
-    console.log("In use...")
+    console.log("[" + (new Date()).toISOString() +  "] " + req.url )
     return next();
+})
+
+app.get('/temp',(req,res)=>{
+  let result = fs.readFileSync('temp.json',{encoding: "utf8"}).toString();
+  res.set("content-type","text/plain")
+  console.log(result);
+  res.send(result);
 })
 
 app.post('*', (req, res)=>{
     console.log(req.url);
+    fs.writeFileSync("temp.json", JSON.stringify(req.body))
     let result = converter(req.body);
 
     fetch(domain + req.url.replace("/" + code , ""), {
